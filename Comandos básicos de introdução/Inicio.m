9a344cb729540f5c8fed133542f8a91d72e23c7e
ClaudioOliveira89 <claudio.h.oliveira.s@gmail.com>
2024-01-14 23:13:54 -0300

    Inicio

---
+++ Comandos básicos de introdução/Inicio.m
@@ -0,0 +1,12 @@
+clc, clear, close
+
+%% Primeira Sessão
+10+30 % Linha para executar uma soma
+
+20+3 % linha para executar uma soma
+
+%% Segunda Sessão
+20-50 % linha para executar uma subtração
+
+10*20 % linha para executar uma multiplicação
+
